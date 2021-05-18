3e92156305f0a2fa25c300235fd8dd7e31fb36ca
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-05-18 13:34:09 -0300

    Completing challenge two of chapter one

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,42 @@
-export function Content() {
+import '../styles/content.scss';
+import { MovieCard } from './MovieCard';
+
+
+interface ContentProps {
+  movies : Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>,
+  selectedGenre: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+   const { movies, selectedGenre } = props;
+
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
+import '../styles/sidebar.scss';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres : Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>,
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number,  
+}
+
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+  const { genres, handleClickButton, selectedGenreId } = props;
+
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map((genre) => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+  )
 }
